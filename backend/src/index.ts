6f9cbb0798525c20ac96896e2fbda44434540445
osmanli42 <osmanli42@github.com>
2026-04-28 19:28:41 +0200

    auto-deploy: update code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,8 +24,8 @@ app.use(cors({
   ],
   credentials: true,
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Health check
 app.get('/api/health', (req, res) => {
