2c63e17cc88752f7a577c2064bb04c3adcfe516c
osmanli42 <osmanli42@github.com>
2026-05-13 13:53:22 +0200

    auto-deploy: update code

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -103,7 +103,7 @@ export default function HomePage() {
           </div>
 
           {/* Search Bar */}
-          <div id="booking" className="animate-slide-up w-full">
+          <div id="booking" className="animate-slide-up w-full" style={{ scrollMarginTop: '90px' }}>
             <SearchBar />
           </div>
         </div>
