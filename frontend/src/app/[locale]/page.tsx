4b70c76bb444342463a7b04ab25bc56cc801c9a1
osmanli42 <osmanli42@github.com>
2026-05-13 22:28:19 +0200

    auto-deploy: update code

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -241,7 +241,8 @@ export default function HomePage() {
                   </div>
                 </div>
               </div>
-            ))}
+              );
+            })}
           </div>
 
           {/* CTA */}
