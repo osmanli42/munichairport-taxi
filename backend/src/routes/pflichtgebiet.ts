68dc3714999c8b80e6b6fce7a976bf7edcdf4d7b
osmanli42 <osmanli42@github.com>
2026-06-18 09:24:20 +0200

    auto-deploy: update code

--- backend/src/routes/pflichtgebiet.ts
+++ backend/src/routes/pflichtgebiet.ts
@@ -20,6 +20,8 @@ interface ConfigRow {
   betriebssitz_enabled: number;
   betriebssitz_lat: number;
   betriebssitz_lng: number;
+  ip_bypass_enabled: number;
+  ip_bypass_distance_km: number;
   updated_at: string;
 }
 
