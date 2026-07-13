68b9b13b45454f7223db6f110e849f45d64328db
osmanli42 <osmanli42@github.com>
2026-07-13 20:12:08 +0200

    auto-deploy: update code

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -29,6 +29,8 @@ export interface BookingFormData {
   phone: string;
   email: string;
   flight_number?: string;
+  flight_validated?: string;
+  flight_info?: string;
   pickup_sign?: string;
   child_seat?: boolean;
   child_seat_details?: string;
