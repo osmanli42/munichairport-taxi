52203e6c25d0372fde84cf8a811f2a49f6290f10
osmanli42 <osmanli42@github.com>
2026-07-26 16:05:10 +0200

    auto-deploy: update code

--- backend/src/routes/auto-discounts.ts
+++ backend/src/routes/auto-discounts.ts
@@ -99,7 +99,7 @@ router.put('/admin/:id', authenticateAdmin, async (req: Request, res: Response):
   try {
     await run(
       `UPDATE auto_discounts SET
-        name=?, discount_percent=?, zone_scope=?, min_km=?, max_km=?, hour_from=?, hour_to=?,
+        name=?, discount_type=?, discount_value=?, zone_scope=?, min_km=?, max_km=?, hour_from=?, hour_to=?,
         weekday_mask=?, booking_index_max=?, max_uses=?, max_discount_amount=?,
         vehicle_types=?, trip_types=?, start_date=?, end_date=?, priority=?, stackable_with_promo=?,
         active=?
